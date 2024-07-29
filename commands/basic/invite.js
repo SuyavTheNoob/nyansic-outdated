afca2df07af50bd9c9cb821199447e12ff8fccb7
SuyavTheNoob <160936754+SuyavTheNoob@users.noreply.github.com>
2024-07-29 06:42:54 +0545

    Update invite.js

--- commands/basic/invite.js
+++ commands/basic/invite.js
@@ -11,9 +11,9 @@ module.exports = {
             .setAuthor({ 
                 name: "Invite Link", 
                 iconURL: "https://cdn.discordapp.com/attachments/1246408947708072027/1256597293323256000/invite.png?ex=668158ed&is=6680076d&hm=030c83f567ffdaf0bebb95e50baaec8bb8a8394fa1b7717cc43c3622447f58e3&" ,
-                 url: "https://discord.gg/xQF9f9yUEM"
+                 url: "https://discord.com/invite/PeEHBqJNbG"
                 })
-            .setDescription('Click [here](https://discord.com/oauth2/authorize?client_id=1248587773309485156) to invite the bot to your server.')
+            .setDescription('Sorry, Nyansic is made only for the sole purpose of this server. If you want an publicly available bot, Please contact cbloxy#0054 on discord. Thank you!')
             .setTimestamp();
 
         await interaction.reply({ embeds: [embed] });
